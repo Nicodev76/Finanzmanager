5a8c04b4fc8822334b7efa845521ae309ffe45f8
Nico <nicolay-schoen@gmx.de>
2026-05-22 20:43:00 +0200

    Responsiv update tabbele

--- Frontend/JS/tabelle.js
+++ Frontend/JS/tabelle.js
@@ -12,10 +12,10 @@ async function tabelleUpdate() {
     daten.forEach(eintrag => {
 
         if(eintrag.betrag < 0){
-            tabelle += "<div class='transactions_header'> <h3 class='datum_h3'>" + eintrag.datum + "</h3> <h3 class='kategorie_h3'>"+eintrag.kategorie+"</h3> <h3 class='betrag_h3_minus'>"+eintrag.betrag+"</h3> <h3 class='aktione_h3'>Aktionen</h3> </div> <hr> ";
+            tabelle += "<div class='transactions_header'> <h3 class='datum_h3'>" + eintrag.datum + "</h3> <h3 class='kategorie_h3'>"+eintrag.kategorie+"</h3> <h3 class='betrag_h3_minus'>"+eintrag.betrag+"</h3> <h3 class='aktione_h3'>🗑️</h3> </div> <hr> ";
         }
         else if(eintrag.betrag > 0){
-            tabelle += "<div class='transactions_header'> <h3 class='datum_h3'>" + eintrag.datum + "</h3> <h3 class='kategorie_h3'>"+eintrag.kategorie+"</h3> <h3 class='betrag_h3_plus'>"+eintrag.betrag+"</h3> <h3 class='aktione_h3'>Aktionen</h3>  </div> <hr> ";
+            tabelle += "<div class='transactions_header'> <h3 class='datum_h3'>" + eintrag.datum + "</h3> <h3 class='kategorie_h3'>"+eintrag.kategorie+"</h3> <h3 class='betrag_h3_plus'>"+eintrag.betrag+"</h3> <h3 class='aktione_h3'>🗑️</h3>  </div> <hr> ";
         }
         
         
