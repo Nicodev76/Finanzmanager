65b64dcabccbc1c4f4eceb36c2335a791444a08c
Nicodev76 <nicolay-schoen@gmx.de>
2026-07-24 21:52:24 +0200

    Dashbord desgin zu ende getslet in form der Grund struktur und es wurden die ertsen js datei erstellt und teilwesie vorberteitet für die funktionalität also um sie dan mit funktioenen auszustaten

---
+++ Frontend/JS/nav.js
@@ -0,0 +1,22 @@
+function nav_main() {
+    // All main objekte werden enfert und werdedn in den einzelnen funktionen wieder angezeigt
+}
+
+function nav_home() {
+    nav_main();
+}
+function nav_analyse() {
+    nav_main();
+}
+function nav_aktien() {
+    nav_main();
+}
+function nav_sparen() {
+    nav_main();
+}
+function nav_reports() {
+    nav_main();
+}
+function nav_mehr() {
+    nav_main();
+}
