1a725ae43c15dede83761043ec0174f719ed3f16
Nicodev76 <nicolay-schoen@gmx.de>
2026-08-20 18:10:45 +0200

    Datenbank erstellt für die ein und ausgaben

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const sqlite3 = require("sqlite3").verbose();
 
-app.use(express.static(path.join(__dirname, 'frontend')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'Index.html'))
+const db = new sqlite3.Database("./Datenbank.db", (err) => {
+  if (err) {
+    console.error(
+      "Es ist ein fehler bei der erstellng der Datenbank passiert",
+      err.message,
+    );
+  } else {
+    console.log("verbunden mit der Datenbank");
+  }
 });
 
-app.listen(3000, () => console.log('Server läuft auf Port 3000'));
+db.serialize(() => {
+  db.run(
+    `CREATE TABLE IF NOT EXISTS finanzdaten (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    typ TEXT NOT NULL,
+    beschreibung TEXT NOT NULL,
+    kategorie TEXT NOT NULL,
+    datum TEXT NOT NULL,
+    betrag REAL NOT NULL,
+    nutzerid INTEGER NOT NULL
+  )`,
+
+    (err) => {
+      if (err) {
+        console.error(
+          "Es ist ein Fehler bei der erstellung der Tabbelle aufgetreten",
+          err.message,
+        );
+      } else {
+        console.log("Die Tabbele wurde erfolgreich erstellt");
+      }
+    },
+  );
+});
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "Index.html"));
+});
+
+app.listen(3000, () => console.log("Server läuft auf Port 3000"));
