f25f28bde1c138215c99296b460e933e08813c3a
Nicodev76 <nicolay-schoen@gmx.de>
2026-08-20 19:25:38 +0200

    Post und get befehle im backend hinzugefügt

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ db.serialize(() => {
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "frontend")));
@@ -47,3 +48,35 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server läuft auf Port 3000"));
+
+
+app.post("/api/finanzdaten", (req, res) => {
+  const {typ, beschreibung, kategorie, datum, betrag, nutzerid} = req.body;
+
+  const sql = "INSERT INTO finanzdaten (typ, beschreibung, kategorie, datum, betrag, nutzerid) VALUES (?,?,?,?,?,?)";
+  
+  db.run(sql, [typ, beschreibung, kategorie, datum, betrag, nutzerid], function (err) {
+    if(err){
+      console.error("Fehler beim speichern", err.message);
+      return res.status(500).json({fehler: "Daten konnten nicht gespeichert werden"});
+    }
+
+    res.json({
+      meldung: "Daten wurden erfolgreich gespeichert", 
+      id: this.lastID,
+    });
+  });
+});
+
+app.get("/api/finanzdaten", (req, res) => {
+  const sql = "SELECT * FROM finanzdaten";
+
+  db.all(sql, [], (err, rows) => {
+    if(err){
+      console.error("fehler beim ausgelessen:", err.message);
+      return res.status(500).json({fehler: "Daten konnten nicht ausgelessen werden"});
+    }
+
+    res.json(rows);
+  })
+})
